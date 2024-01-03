07f5c67202113ca42cd5ac2bdff414cb2cf5b3cf
Jordan Oliver <bus.jordan.r.oliver@gmail.com>
2024-01-03 11:45:46 -0800

    GameDev - Dasher First Commit

---
+++ dasher.cpp
@@ -0,0 +1,17 @@
+//dasher.cpp
+/*
+* Written By: Jordan Oliver
+* Date Created: 1/3/24
+*  This is a C++ side-scrolling game using the RayLib library.
+*  This project will have a win condition, animations, imported assets, and a losing condition.
+*  This project was taken from the GameDev.tv course.
+* Link: github.com/JRoFlo/project2_dasher/
+*/
+
+
+int main()
+{
+
+
+    return 0;
+}
